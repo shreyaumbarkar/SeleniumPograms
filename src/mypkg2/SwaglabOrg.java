c65854b576e200fa87f8da1cd09ad57313f96cb3
shrey <shrey@Shreya>
2024-05-25 08:58:39 +0530

    2nd commit

--- src/mypkg2/SwaglabOrg.java
+++ src/mypkg2/SwaglabOrg.java
@@ -1,10 +1,38 @@
 package mypkg2;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class SwaglabOrg {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","driver//chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.saucedemo.com/v1");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@id=\"user-name\"]"));
+		WebElement name= driver.findElement(By.xpath("//input[@id=\"user-name\"]"));
+		name.sendKeys("standard_user");
+		WebElement pass= driver.findElement(By.xpath("//input[@id=\"password\"]"));
+		pass.sendKeys("secret_sauce");
+		WebElement login= driver.findElement(By.xpath("//input[@class=\"btn_action\"]"));
+		login.click();
+		driver.findElement(By.linkText("Sauce Labs Fleece Jacket")).click();
+		String currentwindowHandle1 = driver.getWindowHandle();
+	    driver.switchTo().window(currentwindowHandle1);
+	    driver.findElement(By.xpath("//*[contains(@class,'svg-inline--fa fa-shopping-cart fa-w-18 fa-3x')]")).click();
+	    driver.findElement(By.linkText("CHECKOUT")).click();
+	    WebElement firstname= driver.findElement(By.xpath("//input[@id=\"first-name\"]"));
+	    firstname.sendKeys("Shreya");
+	    WebElement lastname= driver.findElement(By.xpath("//input[@id=\"last-name\"]"));
+	    lastname.sendKeys("Umbarkar");
+	    WebElement zipcode= driver.findElement(By.xpath("//input[@id=\"postal-code\"]"));
+	    zipcode.sendKeys("444122");
+	    driver.findElement(By.xpath("//input[@class=\"btn_primary cart_button\"]")).click();
+	    driver.findElement(By.linkText("FINISH")).click();
+	    String currentwindowHandle2=driver.getWindowHandle();
+	    driver.switchTo().window(currentwindowHandle2);
+	   
+	    }
 
 }
--- src/mypkg2/WebDriver.java
+++ src/mypkg2/WebDriver.java
@@ -1,14 +1,20 @@
 package mypkg2;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
 public class WebDriver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.setProperty("webdriver.chrome.driver", "driver//chromedriver.exe");
 		ChromeDriver driver= new ChromeDriver();
-		driver.get("https://chatgpt.com");
-		driver.manage().window().maximize();
-		driver.findElement(By.className("//*[@id=\"radix-:r7:\"]/div/div/button[2]/div")).click();
+		driver.get("https://www.instagram.com");
+		WebElement logo=driver.findElement(By.xpath("//div[@class=\"x78zum5 xdt5ytf\"]"));
+		File f= logo.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(f, new File("C:\\screenshot.png"));
 		
 	}
 		
